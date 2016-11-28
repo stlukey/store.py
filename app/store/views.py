c5753d943a081bdf77d539240a39cafc3bc9dffa
Luke Southam <luke@devthe.com>
2016-11-28 04:02:02 +0000

    Rendering products in templates.

--- app/store/views.py
+++ app/store/views.py
@@ -2,16 +2,36 @@
 """
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, make_response
+from bson.objectid import ObjectId
+from pprint import pformat
 
 store = Blueprint('store', __name__)
 
 
+@store.route('/images/<product_id>/<file_name>')
+def images(product_id, file_name):
+    product = current_app.db.Product.get_from_id(ObjectId(product_id))
+    with product.fs.get_last_version('images/{}'.format(file_name)) as f:
+        response  = make_response(f.read())
+    response.mimetype = 'image/jpeg'
+    return response
+
+
 @store.route('/product/<pid>')
 def display_product(pid):
-    return "Displaying product: {}".format(pid)
+    product_id = ObjectId(pid)
+    product = current_app.db.Product.get_from_id(product_id)
+    categories = [c.category['name'] for c in current_app.db.ProductToCategory.find({'product._id': product_id})]
+    return render_template('store/product.html', product=pformat(product, indent=4), categories=pformat(categories, indent=4))
 
 
 @store.route('/')
 def store_front():
-    return render_template('store/index.html')
+    popular = []
+    while len(popular) < 4:
+        product = current_app.db.Product.find_random()
+        if product not in popular:
+            popular.append(product)
+
+    return render_template('store/index.html', most_popular=popular)
--- data.py
+++ data.py
@@ -18,7 +18,8 @@ def drop_db():
 
 
 def get_products():
-    product_files = ['data/' + f[:-4] for f in os.listdir('data') if f.endswith('json')]
+    product_files = ['data/' + f[:-4]
+                     for f in os.listdir('data') if f.endswith('json')]
     products = []
     for product_file in product_files:
         with open(product_file + 'json') as f:
@@ -28,12 +29,14 @@ def get_products():
 
     return products
 
-def copy_image(src, dest, length=16*1024):
+
+def copy_image(src, dest, length=16 * 1024):
     while True:
         buf = src.read(length)
         if not buf:
             break
         dest.write(buf)
+
 
 @manager.command
 def create_sample():
@@ -50,16 +53,28 @@ def create_sample():
         product.links = product_info['links']
         product.save()
 
+        for category_name in product_info['categories']:
+            category = app.db.Category.one({'name':category_name})
+            if category is None:
+                category = app.db.Category()
+                category.name = category_name
+                category.save()
+            product_to_category = app.db.ProductToCategory()
+            product_to_category.product = product
+            product_to_category.category = category
+            product_to_category.save()
+
         with open(product_info['image'], 'rb') as src:
-            with product.fs.new_file('thumbnail') as dest:
+            with product.fs.new_file('images/thumbnail.jpg'.format(product._id)) as dest:
                 copy_image(src, dest)
 
-            with product.fs.new_file('images/image1.jpg') as dest:
+            with product.fs.new_file('images/image1.jpg'.format(product._id)) as dest:
                 copy_image(src, dest)
 
         print("Adding '{}'...".format(product.name))
         product.save()
     print("Complete!")
+
 
 if __name__ == '__main__':
     manager.run()
