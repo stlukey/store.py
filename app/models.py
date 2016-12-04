stlukey/store.py

commit c53b003a00b7d05a50e1b1f832579c0578212427

Restructured project.

app/models.py
@@ -1,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Data model.
+"""
+
+from mongokit import Document
+from importlib import import_module
+
+to_register = []
+
+
+def db_register(doc):
+    if doc not in to_register:
+        to_register.append(doc)
+
+
+class BaseDocument(Document):
+    __database__ = 'store'
+    __collection__ = 'store'
+    use_dot_notation = True
+
+
+class AutorefsDocument(BaseDocument):
+    user_autorefs = True
+
+
+def init_db_docs(app):
+    import_module('app.modules')
+    for doc in to_register:
+        print("Registering Document:", doc.__name__)
+        app.db.register(doc)

app/modules/admin/models.py
@@ -1,0 +1,15 @@
+
+from datetime import datetime
+
+from ...models import BaseDocument, db_register
+
+@db_register
+class Shipment(BaseDocument):
+    __collections__ = 'shipments'
+    structure = {
+        'create_time': datetime,
+        'dispatch_time': datetime
+    }
+    default_values = {
+        'create_time': datetime.now
+    }

app/modules/checkout/models.py
@@ -1,0 +1,47 @@
+from ...models import AutorefsDocument, db_register
+
+from datetime import datetime
+
+from ..users.models import User, Address
+from ..admin.models import Shipment
+
+from ..product.models import Product
+
+
+@db_register
+class Order(AutorefsDocument):
+    __collections__ = 'orders'
+    structure = {
+        'user': User,
+        'datetime': datetime,
+        'payment': {
+            'address': Address,
+            'amount': float,
+            'currency': str,
+            'ref': str,
+            'datetime': datetime
+        },
+        'shipping': {
+            'address': Address,
+            'method': int,
+            'shipment': Shipment,
+            'tracking_id': str,
+        },
+        'items': [{
+            'product': Product,
+            'quantity': int,
+        }]
+    }
+    required_feilds = [
+        'user',
+        'datetime',
+        'payment',
+        'shipping.address',
+        'shipping.method',
+        'items'
+    ]
+    default_values = {
+        'datetime': datetime.now,
+        'payment.datetime': datetime.now,
+        'payment.currency': 'GBP',
+    }

app/modules/users/models.py
@@ -1,0 +1,59 @@
+from ...models import BaseDocument, AutorefsDocument, db_register
+from datetime import datetime
+
+
+@db_register
+class User(BaseDocument):
+    __collection__ = 'users'
+    structure = {
+        'email': str,
+        'password': str,
+        'first_name': str,
+        'last_name': str,
+        'contact_num': str,
+        'create_time': datetime
+    }
+    default_values = {
+        'create_time': datetime.now
+    }
+    required_feilds = [
+        'email',
+        'password',
+        'first_name',
+        'last_name',
+        'contact_num'
+    ]
+
+
+@db_register
+class Address(AutorefsDocument):
+    __collection__ = 'addresses'
+    structure = {
+        'user': User,
+        'full_name': str,
+        'line1': str,
+        'line2': str,
+        'line3': str,
+        'city': str,
+        'county': str,
+        'postcode': str
+    }
+    required_feilds = {
+        'user',
+        'full_name',
+        'line1',
+        'city',
+        'county',
+        'postcode'
+    }
+
+
+@db_register
+class DefaultAddresses(AutorefsDocument):
+    __collection__ = 'default_addresses'
+    structure = {
+        'user': User,
+        'billing': Address,
+        'shipping': Address
+    }
+    required_feilds = structure.keys()

app/modules/product/models.py
@@ -1,0 +1,47 @@
+
+from ...models import BaseDocument, AutorefsDocument, db_register
+from datetime import datetime
+
+
+@db_register
+class Product(BaseDocument):
+    __collection__ = 'products'
+    structure = {
+        'name': str,
+        'cost': float,
+        'date_added': datetime,
+        'description': str,
+        'stock': int,
+        'recipes': [{
+            'name': str,
+            'url': str
+        }]
+    }
+    default_feilds = {
+        'date_added': datetime.now
+    }
+    required_feilds = structure.keys()
+    gridfs = {
+        'containers': [
+            'images'
+        ]
+    }
+
+
+@db_register
+class Category(BaseDocument):
+    __collection__ = 'categories'
+    structure = {
+        'name': str
+    }
+    required_feilds = ['name']
+
+
+@db_register
+class ProductToCategory(AutorefsDocument):
+    __collection__ = 'products_to_categories'
+    structure = {
+        'product': Product,
+        'category': Category
+    }
+    required_feilds = structure.keys()

app/store/models.py
@@ -1,176 +1,0 @@
-#!/usr/bin/env python
-"""
-Data model.
-"""
-
-from mongokit import Document
-from datetime import datetime
-
-to_register = []
-
-
-def register_doc(document):
-    to_register.append(document)
-    return document
-
-
-class BaseDocument(Document):
-    __database__ = 'store'
-    __collection__ = 'store'
-    use_dot_notation = True
-
-
-class AutorefsDocument(BaseDocument):
-    user_autorefs = True
-
-
-@register_doc
-class User(BaseDocument):
-    __collection__ = 'users'
-    structure = {
-        'email': str,
-        'password': str,
-        'first_name': str,
-        'last_name': str,
-        'contact_num': str,
-        'create_time': datetime
-    }
-    default_values = {
-        'create_time': datetime.now
-    }
-    required_feilds = [
-        'email',
-        'password',
-        'first_name',
-        'last_name',
-        'contact_num'
-    ]
-
-
-@register_doc
-class Address(AutorefsDocument):
-    __collection__ = 'addresses'
-    structure = {
-        'user': User,
-        'full_name': str,
-        'line1': str,
-        'line2': str,
-        'line3': str,
-        'city': str,
-        'county': str,
-        'postcode': str
-    }
-    required_feilds = {
-        'user',
-        'full_name',
-        'line1',
-        'city',
-        'county',
-        'postcode'
-    }
-
-
-@register_doc
-class DefaultAddresses(AutorefsDocument):
-    __collection__ = 'default_addresses'
-    structure = {
-        'user': User,
-        'billing': Address,
-        'shipping': Address
-    }
-    required_feilds = structure.keys()
-
-
-@register_doc
-class Product(BaseDocument):
-    __collection__ = 'products'
-    structure = {
-        'name': str,
-        'cost': float,
-        'date_added': datetime,
-        'description': str,
-        'stock': int,
-        'recipes': [{
-            'name': str,
-            'url': str
-        }]
-    }
-    default_feilds = {
-        'date_added': datetime.now
-    }
-    required_feilds = structure.keys()
-    gridfs = {
-        'containers': [
-            'images'
-        ]
-    }
-
-
-@register_doc
-class Category(BaseDocument):
-    __collection__ = 'categories'
-    structure = {
-        'name': str
-    }
-    required_feilds = ['name']
-
-
-@register_doc
-class ProductToCategory(AutorefsDocument):
-    __collection__ = 'products_to_categories'
-    structure = {
-        'product': Product,
-        'category': Category
-    }
-    required_feilds = structure.keys()
-
-
-@register_doc
-class Shipment(BaseDocument):
-    __collections__ = 'shipments'
-    structure = {
-        'create_time': datetime,
-        'dispatch_time': datetime
-    }
-    default_values = {
-        'create_time': datetime.now
-    }
-
-
-@register_doc
-class Order(AutorefsDocument):
-    __collections__ = 'orders'
-    structure = {
-        'user': User,
-        'datetime': datetime,
-        'payment': {
-            'address': Address,
-            'amount': float,
-            'currency': str,
-            'ref': str,
-            'datetime': datetime
-        },
-        'shipping': {
-            'address': Address,
-            'method': int,
-            'shipment': Shipment,
-            'tracking_id': str,
-        },
-        'items': [{
-            'product': Product,
-            'quantity': int,
-        }]
-    }
-    required_feilds = [
-        'user',
-        'datetime',
-        'payment',
-        'shipping.address',
-        'shipping.method',
-        'items'
-    ]
-    default_values = {
-        'datetime': datetime.now,
-        'payment.datetime': datetime.now,
-        'payment.currency': 'GBP',
-    }

app/__init__.py
@@ -9,6 +9,8 @@ from flask import Flask
 from flask_assets import Environment
 from .config import DevelopmentConfig
 
+from .models import init_db_docs
+
 
 def create_app(config=DevelopmentConfig):
     app = Flask(__name__)
@@ -23,26 +25,25 @@ def create_app(config=DevelopmentConfig):
 
 def setup_mongodb(app):
     from mongokit import Connection
-    from .store.models import to_register
 
     app.db = Connection(app.config['MONGODB_HOST'],
                         app.config['MONGODB_PORT'])
 
-    for document in to_register:
-        app.db.register(document)
+    init_db_docs(app)
 
 
 def setup_assets(app):
     app.env = Environment(app)
     app.env.load_path = [
-        ospath.join(ospath.dirname(__file__), 'less'),
-        ospath.join(ospath.dirname(__file__), 'coffee'),
-        ospath.join(ospath.dirname(__file__), 'bower_components'),
+        ospath.join(ospath.dirname(__file__), 'static/less'),
+        ospath.join(ospath.dirname(__file__), 'static/coffee'),
+        ospath.join(ospath.dirname(__file__), 'static/bower_components'),
     ]
 
 
 def register_blueprints(app):
-    from .store import store
+    from .views import store as store_bp
+    from .modules.product import product_bp
 
-    for bp in [store]:
-        app.register_blueprint(bp)
+    app.register_blueprint(store_bp)
+    app.register_blueprint(product_bp, url_prefix='/products')

app/modules/__init__.py
@@ -1,0 +1,5 @@
+from . import admin
+from . import checkout
+from . import users
+from . import product
+__all__ = ['admin', 'checkout', 'product', 'users']

commit 2895381899c971f73dffe9d79a6835e335633911

Fixed database models that c53b003 broke.

app/models.py
@@ -6,12 +6,22 @@ Data model.
 from mongokit import Document
 from importlib import import_module
 
-to_register = []
+conns = []
 
 
-def db_register(doc):
-    if doc not in to_register:
-        to_register.append(doc)
+class PartialConnection(object):
+    def __init__(self):
+        conns.append(self)
+        self.documents = []
+
+    def register(self, doc):
+        self.documents.append(doc)
+        return doc
+
+    def complete(self, connection):
+        for doc in self.documents:
+            print("Registering Document:", doc.__name__)
+            connection.register(doc)
 
 
 class BaseDocument(Document):
@@ -26,6 +36,5 @@ class AutorefsDocument(BaseDocument):
 
 def init_db_docs(app):
     import_module('app.modules')
-    for doc in to_register:
-        print("Registering Document:", doc.__name__)
-        app.db.register(doc)
+    for conn in conns:
+        conn.complete(app.db)

app/modules/admin/models.py
@@ -1,9 +1,11 @@
 
 from datetime import datetime
 
-from ...models import BaseDocument, db_register
+from ...models import BaseDocument, PartialConnection
 
-@db_register
+conn = PartialConnection()
+
+@conn.register
 class Shipment(BaseDocument):
     __collections__ = 'shipments'
     structure = {

app/modules/checkout/models.py
@@ -1,4 +1,4 @@
-from ...models import AutorefsDocument, db_register
+from ...models import AutorefsDocument, PartialConnection
 
 from datetime import datetime
 
@@ -7,8 +7,9 @@ from ..admin.models import Shipment
 
 from ..products.models import Product
 
+conn = PartialConnection()
 
-@db_register
+@conn.register
 class Order(AutorefsDocument):
     __collections__ = 'orders'
     structure = {

app/modules/products/models.py
@@ -1,9 +1,11 @@
 
-from ...models import BaseDocument, AutorefsDocument, db_register
+from ...models import PartialConnection, BaseDocument, AutorefsDocument
 from datetime import datetime
 
+conn = PartialConnection()
 
-@db_register
+
+@conn.register
 class Product(BaseDocument):
     __collection__ = 'products'
     structure = {
@@ -32,7 +34,7 @@ class Product(BaseDocument):
         return cls.find(cls)
 
 
-@db_register
+@conn.register
 class Category(BaseDocument):
     __collection__ = 'categories'
     structure = {
@@ -41,7 +43,7 @@ class Category(BaseDocument):
     required_feilds = ['name']
 
 
-@db_register
+@conn.register
 class ProductToCategory(AutorefsDocument):
     __collection__ = 'products_to_categories'
     structure = {

app/modules/users/models.py
@@ -1,8 +1,9 @@
-from ...models import BaseDocument, AutorefsDocument, db_register
+from ...models import BaseDocument, AutorefsDocument, PartialConnection
 from datetime import datetime
 
+conn = PartialConnection()
 
-@db_register
+@conn.register
 class User(BaseDocument):
     __collection__ = 'users'
     structure = {
@@ -25,7 +26,7 @@ class User(BaseDocument):
     ]
 
 
-@db_register
+@conn.register
 class Address(AutorefsDocument):
     __collection__ = 'addresses'
     structure = {
@@ -48,7 +49,7 @@ class Address(AutorefsDocument):
     }
 
 
-@db_register
+@conn.register
 class DefaultAddresses(AutorefsDocument):
     __collection__ = 'default_addresses'
     structure = {