1f6ccb2c979a17fab0ea6df3a59722c104573e41
Luke Southam <luke@devthe.com>
2019-05-19 23:07:58 +0100

    Update models.py

--- api/resources/mailing_list/models.py
+++ api/resources/mailing_list/models.py
@@ -5,15 +5,21 @@ from ..users.models import User
 from ...database import db, Document
 
 class Subscriber(Document):
+    # Collection name.
     _collection = db.subscribers
+    
+    # Document Feilds.
     _schema = [
         'email',
         'datetime'
     ]
+    
+    # Required Feilds.
     _check = [
         'email'
     ]
 
+    # On creation, add date.
     @staticmethod
     def _format_new(**kwargs):
         kwargs['datetime'] = datetime.now()
